71b2ee12593cfc58e2d56669db933e47e36c4197
shubhamshinde17 <shubhamshinde266@gmail.com>
2021-11-09 22:44:10 +0530

    ..

--- src/main/java/com/api/api/ApiApplication.java
+++ src/main/java/com/api/api/ApiApplication.java
@@ -2,9 +2,7 @@ package com.api.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
 @SpringBootApplication
 public class ApiApplication {
 
--- src/main/java/com/api/api/controllers/AuthController.java
+++ src/main/java/com/api/api/controllers/AuthController.java
@@ -16,11 +16,13 @@ import com.api.api.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RequestMapping(value = "api/auth")
 @RestController
 public class AuthController {
